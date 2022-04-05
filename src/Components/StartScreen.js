1feb2aed1004e42967d2fb9abd8a047202d8ec3e
bikeshrai78 <bikeshrai78@gmail.com>
2022-04-05 21:48:27 +0545

    Style start page

---
+++ src/Components/StartScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function StartScreen() {
+  return (
+    <div className="page-start">
+      <div className="header">
+        <h3>Quizzical</h3>
+      </div>
+      <button className="btn-primary">Start Quiz</button>
+    </div>
+  );
+}
